b3636f76c1c1d9985709af98bab05b80b4d457dd
HelderDePegaso <115958521+HelderDePegaso@users.noreply.github.com>
2026-01-02 20:11:16 +0100

    feat: Serve areas with last modified aproche

--- src/shared/service/user-area/user-area.service.ts
+++ src/shared/service/user-area/user-area.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Area } from 'src/entities/areas/model/area.model';
-import { User } from 'src/entities/users/model/user.model'; 
+import { User } from 'src/entities/users/model/user.model';
 import { UserArea } from 'src/shared/model/user-area.model';
 import { InjectModel } from '@nestjs/sequelize';
 import { UsersService } from 'src/entities/users/users.service';
@@ -10,45 +10,47 @@ import { AreasService } from 'src/entities/areas/areas.service';
 import { v4 as uuidv4 } from 'uuid';
 import { Promotion } from 'src/shared/model/promotion.model';
 import { Role } from 'src/shared/model/role.model';
+import { Op } from 'sequelize';
+import { formatSQLDateUTC } from 'src/utils/util';
 
-@Injectable() 
+@Injectable()
 export class UserAreaService {
-    
+
 
     //@InjectModel(UserArea) private userArea : UserArea;
-    private userArea : typeof UserArea
-    
-    constructor( private userService: UsersService,  private areaService: AreasService) {}
+    private userArea: typeof UserArea
 
-    
+    constructor(private userService: UsersService, private areaService: AreasService) { }
+
+
 
     async addUserToArea(userModelOrUuid: User | string, areaModelOrUuid: Area | string) {
-        const loadedUser: User | null = userModelOrUuid instanceof User ? userModelOrUuid : await this.userService.get({uuid: userModelOrUuid}) ;
-        const loadedArea: Area | null = areaModelOrUuid instanceof Area ? areaModelOrUuid : await this.areaService.get({uuid: areaModelOrUuid})
+        const loadedUser: User | null = userModelOrUuid instanceof User ? userModelOrUuid : await this.userService.get({ uuid: userModelOrUuid });
+        const loadedArea: Area | null = areaModelOrUuid instanceof Area ? areaModelOrUuid : await this.areaService.get({ uuid: areaModelOrUuid })
 
         if (!loadedUser || !loadedArea) {
             return null;
-        } 
+        }
         console.log("Vssssssss");
-        return await UserArea.create({user_id: loadedUser.id, area_id: loadedArea.id, uuid: uuidv4()})
+        return await UserArea.create({ user_id: loadedUser.id, area_id: loadedArea.id, uuid: uuidv4() })
 
-         
+
     }
 
     async removeUserFromArea(userModelOrUuid: User | string, areaModelOrUuid: Area | string) {
         try {
-            const loadedUser: User | null = userModelOrUuid instanceof User ? userModelOrUuid : await this.userService.get({uuid: userModelOrUuid}) ;
-            const loadedArea: Area | null = areaModelOrUuid instanceof Area ? areaModelOrUuid : await this.areaService.get({uuid: areaModelOrUuid})
+            const loadedUser: User | null = userModelOrUuid instanceof User ? userModelOrUuid : await this.userService.get({ uuid: userModelOrUuid });
+            const loadedArea: Area | null = areaModelOrUuid instanceof Area ? areaModelOrUuid : await this.areaService.get({ uuid: areaModelOrUuid })
 
             console.log("loadeds")
             console.log(loadedUser)
             console.log(loadedArea)
-     
+
             if (!loadedUser || !loadedArea) {
                 return null;
-            } 
-            
-            const userArea = await this.get({user_id: loadedUser.id, area_id: loadedArea.id})
+            }
+
+            const userArea = await this.get({ user_id: loadedUser.id, area_id: loadedArea.id })
 
             console.log("What was deleted")
             console.log(userArea)
@@ -62,23 +64,31 @@ export class UserAreaService {
         }
     }
 
-    async get(where: Partial<UserArea>) {   
+    async get(where: Partial<UserArea>) {
         console.log(this.userArea)
-        return await UserArea.findOne({where})
+        return await UserArea.findOne({ where })
     }
 
-    async getAllUserAreas(userId: number) {
+    async getAllUserAreas(userId: number, lastModified: number = 0 /** timestamp */) {
+        
+        const areaWhere = { updated_at: { [Op.gt]: formatSQLDateUTC(lastModified) } };
+        const area: any = {
+            model: Area,
+            attributes: ["uuid", "name", "abbrev", "status", "domain_id"]
+        }
+
+        if (lastModified > 0) area.where = areaWhere
+        
         return await UserArea.findAll({
-            attributes: ['uuid'] ,
-            where    :   {
+            attributes: ['uuid'],
+            where: {
                 user_id: userId
-            } , 
+            },
 
             include: [
-                {
-                    model: Area,
-                    attributes: ["uuid" , "name" , "abbrev" ,  "status" , "domain_id"]
-                }, 
+
+                area,
+
 
                 {
                     model: Promotion,
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,4 +1,5 @@
 export *  from './role-logic'
 export *  from './omitFields'
 export *  from './changeFieldName'
+export *  from './format-sqldate-utc'
 
